a3d1d27283afc172475f671221d666765fff38e0
RofLANi4 <artemclassics19@gmail.com>
2024-06-05 02:08:08 +0300

    Complete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,9 +13,6 @@ const io = socketIO(server, {
   },
 });
 
-//http://26.117.70.106:3000
-//http://localhost:3000
-
 app.use(cors());
 
 app.use(express.static("public"));
@@ -23,19 +21,367 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("Пользователь подключен");
+let words = [];
 
-  socket.on("updateCount", (value) => {
-    socket.broadcast.emit("updateValue", value);
+// Створення схеми для слова з асоціаціями
+const wordSchema = new mongoose.Schema({
+  word: String,
+  associations: [String], // Масив асоціацій для кожного слова
+});
+
+// Модель для слова
+const Word = mongoose.model("Word", wordSchema);
+
+// Підключення до бази даних
+mongoose
+  .connect(
+    "mongodb+srv://Zhovtovatiuk:12345@cluster0.fjkkmek.mongodb.net/Codenames",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Підключення до бази даних успішне");
+    updateWords();
+  })
+  .catch((err) => console.error("Помилка підключення до бази даних:", err));
+
+// Оновлення списку слів
+async function updateWords() {
+  try {
+    // Отримання всіх слів з бази даних
+    const wordsFromDB = await Word.find({});
+
+    // Формування масиву слів з асоціаціями
+    words = wordsFromDB.map((wordObj) => ({
+      word: wordObj.word,
+      associations: wordObj.associations,
+    }));
+    // Використання масиву слів для подальшої роботи
+  } catch (error) {
+    console.error("Помилка при отриманні слів з бази даних:", error);
+  }
+}
+
+let gameState = {
+  users: [],
+};
+
+const shuffleArray = (array) => {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
+
+// Добавьте это состояние где-то выше в вашем коде, чтобы хранить предыдущие ассоциации
+let previousAssociations = new Set();
+
+function selectAssociation(activeTeam) {
+  const currentTeam = activeTeam;
+  const unguessedWords = gameState.coloredWords.filter(
+    (word) => word.guessed === false && word.color === currentTeam
+  );
+
+  // Словарь для подсчета частот ассоциаций
+  let associationFrequency = {};
+
+  unguessedWords.forEach((wordObj) => {
+    wordObj.word.associations.forEach((assoc) => {
+      if (associationFrequency[assoc]) {
+        associationFrequency[assoc]++;
+      } else {
+        associationFrequency[assoc] = 1;
+      }
+    });
+  });
+
+  // Найти самую частую ассоциацию, которая не была использована ранее
+  let maxFrequency = 0;
+  let bestAssociation = null;
+
+  for (let assoc in associationFrequency) {
+    if (
+      associationFrequency[assoc] > maxFrequency &&
+      !previousAssociations.has(assoc)
+    ) {
+      maxFrequency = associationFrequency[assoc];
+      bestAssociation = assoc;
+    }
+  }
+
+  // Если нет частых ассоциаций, выбрать первую ассоциацию из первого неотгаданного слова
+  if (!bestAssociation && unguessedWords.length > 0) {
+    for (let wordObj of unguessedWords) {
+      for (let assoc of wordObj.word.associations) {
+        if (!previousAssociations.has(assoc)) {
+          bestAssociation = assoc;
+          break;
+        }
+      }
+      if (bestAssociation) break;
+    }
+  }
+
+  // Если так и не нашлось новой ассоциации, возможно стоит пересмотреть стратегию или начинать с чистого листа
+  if (!bestAssociation) {
+    previousAssociations.clear();
+    bestAssociation = unguessedWords[0].word.associations[0];
+  }
+
+  // Добавить выбранную ассоциацию в предыдущие ассоциации
+  previousAssociations.add(bestAssociation);
+  console.log(bestAssociation, maxFrequency);
+
+  return `${bestAssociation} ${maxFrequency}`;
+}
+
+function generateColoredWords(
+  words,
+  blueCount,
+  redCount,
+  whiteCount,
+  blackCount
+) {
+  const coloredWords = [];
+  const wordColors = [];
+
+  // Генерируем индексы для синих слов
+  for (let i = 0; i < blueCount; i++) {
+    let index;
+    do {
+      index = Math.floor(Math.random() * words.length);
+    } while (wordColors[index] !== undefined); // Проверяем, что слово еще не было выбрано
+    wordColors[index] = "blue";
+  }
+
+  // Генерируем индексы для красных слов
+  for (let i = 0; i < redCount; i++) {
+    let index;
+    do {
+      index = Math.floor(Math.random() * words.length);
+    } while (wordColors[index] !== undefined); // Проверяем, что слово еще не было выбрано
+    wordColors[index] = "red";
+  }
+
+  // Генерируем индексы для белых слов
+  for (let i = 0; i < whiteCount; i++) {
+    let index;
+    do {
+      index = Math.floor(Math.random() * words.length);
+    } while (wordColors[index] !== undefined); // Проверяем, что слово еще не было выбрано
+    wordColors[index] = "white";
+  }
+
+  // Генерируем индексы для черных слов
+  for (let i = 0; i < blackCount; i++) {
+    let index;
+    do {
+      index = Math.floor(Math.random() * words.length);
+    } while (wordColors[index] !== undefined); // Проверяем, что слово еще не было выбрано
+    wordColors[index] = "black";
+  }
+
+  // Заполняем массив coloredWords цветными словами
+  for (let i = 0; i < words.length; i++) {
+    coloredWords.push({
+      word: words[i],
+      color: wordColors[i],
+      guessed: false,
+    });
+  }
+
+  return coloredWords;
+}
+
+let timer = 120;
+let intervalId;
+let method = "add";
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("joinMaster", (team, nickname, userId) => {
+    const master = { id: userId, nickname, team, role: "master" };
+    if (userId === "redBot") {
+      setTimeout(() => {
+        if (!gameState.associations) {
+          gameState.associations = { red: [], blue: [] };
+        }
+        gameState.associations["red"].push(selectAssociation("red"));
+        socket.emit("updateGameState", gameState);
+        startTimer();
+      }, 5000);
+    }
+    gameState.users.push(master);
+    socket.emit("updateGameState", gameState);
+  });
+
+  socket.on("startGame", () => {
+    gameState.isGameStarted = true;
+
+    const blueCount = 10;
+    const redCount = 9;
+    const whiteCount = 4;
+    const blackCount = 2;
+
+    const coloredWords = generateColoredWords(
+      shuffleArray(words).slice(0, 25),
+      blueCount,
+      redCount,
+      whiteCount,
+      blackCount
+    );
+
+    gameState.coloredWords = coloredWords;
+
+    io.emit("updateGameState", gameState);
+    io.emit("gameStarted");
+  });
+
+  socket.on("restartGame", () => {
+    gameState.users.length = 0;
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    gameState.associations = { red: [], blue: [] };
+
+    timer = 120;
+    io.emit("updateActiveTeam", "red");
+    io.emit("gameRestarted", timer);
+    io.emit("updateGameState", gameState);
+  });
+
+  socket.on("joinPlayer", (team, nickname, userId) => {
+    const player = { id: userId, nickname, team, role: "player" };
+    gameState.users.push(player);
+    socket.emit("updateGameState", gameState);
+    socket.broadcast.emit("updateGameState", gameState);
   });
 
   socket.on("disconnect", () => {
-    console.log("Пользователь отключен");
+    console.log("User disconnected");
   });
+
+  socket.on("buttonClicked", (id) => {
+    io.emit("clickReaction", id, method);
+  });
+  socket.on("changeMasterControle", (masterControle) => {
+    io.emit("changeMasterControle", masterControle);
+  });
+
+  socket.on(
+    "toggleWordSelection",
+    (gameState, index, activeUsers, activeTeam) => {
+      const word = gameState.coloredWords[index];
+      if (word) {
+        if (activeUsers.indexOf(socket.id) !== -1) {
+          if (word.selectedCount > 0) {
+            word.selectedCount--;
+          } else word.selectedCount = 0;
+        } else {
+          word.selectedCount++;
+          if (
+            word.selectedCount ===
+            gameState.users.filter(
+              (user) => user.role === "player" && user.team === activeTeam
+            ).length
+          ) {
+            word.guessed = true;
+            word.selectedCount = 0;
+            if (word.color === activeTeam) {
+              io.emit("updateGameState", gameState);
+              timer += 15;
+              method = "remove";
+              return;
+            } else if (word.color === "white") {
+              changeActiveTeam(activeTeam, gameState);
+              method = "remove";
+              return;
+            } else if (word.color === "black") {
+              endGame();
+              return;
+            } else {
+              changeActiveTeam(activeTeam, gameState);
+              method = "remove";
+              return;
+            }
+          }
+        }
+
+        io.emit("updateGameState", gameState);
+
+        function endGame() {
+          gameState.coloredWords.forEach((word) => (word.guessed = true));
+          if (intervalId) {
+            clearInterval(intervalId);
+          }
+          io.emit("updateGameState", gameState);
+        }
+      }
+    }
+  );
+  function changeActiveTeam(activeTeam, gameState) {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    timer = 120;
+    if (activeTeam === "red") {
+      activeTeam = "blue";
+    } else activeTeam = "red";
+    if (
+      gameState.users &&
+      gameState.users.length > 0 &&
+      gameState.users[0].id.includes("Bot")
+    ) {
+      setTimeout(() => {
+        gameState.associations[activeTeam].push(selectAssociation(activeTeam));
+        socket.emit("updateGameState", gameState);
+      }, 5000);
+    }
+    gameState.coloredWords.forEach((word) => (word.selectedCount = 0));
+    io.emit("updateActiveTeam", activeTeam, timer);
+    io.emit("updateGameState", gameState);
+    io.emit("changeMasterControle", true);
+    startTimer();
+  }
+  socket.on("submitAssociation", (gameState, associationWord, color) => {
+    if (!gameState.associations) {
+      gameState.associations = { red: [], blue: [] };
+    }
+    gameState.associations[color].push(associationWord);
+    io.emit("updateGameState", gameState);
+  });
+
+  socket.on("changeActiveTeam", (activeTeam, gameState, methodParam) => {
+    method = methodParam;
+    changeActiveTeam(activeTeam, gameState);
+  });
+
+  socket.on("firstAssociation", (gameStates) => {
+    gameState = gameStates;
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    timer = 120;
+    startTimer();
+  });
+
+  function startTimer() {
+    intervalId = setInterval(() => {
+      timer--;
+      io.emit("restartTime", timer);
+      if (timer === 0) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  }
 });
 
-const port = process.env.PORT || 4000;
-server.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,42 +1,439 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
-import { io } from "socket.io-client";
+import io from "socket.io-client";
+import "./App.css";
 
 const socket = io("http://26.117.70.106:4000");
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
 
-//http://26.117.70.106:4000
-//http://localhost:4000
+socket.on("disconnect", () => {
+  console.log("Disconnected from server");
+});
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    const newValue = count + 1;
-    setCount(newValue);
-    socket.emit("updateCount", newValue);
-  };
-
-  const clearCount = () => {
-    setCount(0);
-    socket.emit("updateCount", 0);
-  };
+  const [gameState, setGameState] = useState(null);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [activeUsers, setActiveUsers] = useState([]);
+  const [associationWord, setAssociationWord] = useState("");
+  const [activeTeam, setActiveTeam] = useState("red");
+  const [showMasterControls, setShowMasterControls] = useState(true);
+  const [timer, setTimer] = useState(120);
 
   useEffect(() => {
-    console.log("useEffect запущен");
-    socket.on("updateValue", (value) => {
-      console.log("Новое знчение:", value);
-      setCount(value);
+    const userId = localStorage.getItem("userId");
+    if (!userId && socket.id) {
+      localStorage.setItem("userId", socket.id);
+    } else if (localStorage.getItem("userId") !== socket.id) {
+      socket.id = localStorage.getItem("userId");
+    }
+  }, []);
+
+  useEffect(() => {
+    socket.on("updateGameState", (gameState) => {
+      if (gameState !== null) {
+        setGameState(gameState);
+      }
     });
     return () => {
-      socket.off("updateCount");
+      socket.off("updateGameState");
     };
   }, []);
 
+  useEffect(() => {
+    socket.on("gameStarted", () => {
+      setIsGameStarted(true);
+    });
+    return () => {
+      socket.off("gameStarted");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("gameRestarted", (time) => {
+      setTimer(time);
+      setIsGameStarted(false);
+    });
+    return () => {
+      socket.off("gameRestarted");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("clearActiveUsers", (activeUsers) => {
+      setActiveUsers(activeUsers);
+    });
+    return () => {
+      socket.off("clearActiveUsers");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("clickReaction", (userId, method) => {
+      if (method === "remove") {
+        setActiveUsers([]);
+      } else
+        setActiveUsers((prevActiveUsers) => {
+          if (prevActiveUsers.includes(userId)) {
+            return prevActiveUsers.filter((user) => user !== userId);
+          } else {
+            return [...prevActiveUsers, userId];
+          }
+        });
+    });
+
+    return () => {
+      socket.off("clickReaction");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("updateActiveTeam", (team) => {
+      setActiveTeam(team);
+    });
+    return () => {
+      socket.off("updateActiveTeam");
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleRestartTime = (time) => {
+      if (time === 0) {
+        socket.emit("changeActiveTeam", activeTeam, gameState, "add");
+      }
+      setTimer(time);
+    };
+
+    socket.on("restartTime", handleRestartTime);
+
+    return () => {
+      socket.off("restartTime", handleRestartTime);
+    };
+  }, [activeTeam, gameState]);
+
+  const joinMaster = (team, playerOrBot) => {
+    if (playerOrBot === "player") {
+      const nickname = prompt("Введіть нік:");
+      if (nickname !== null && nickname !== "") {
+        socket.emit("joinMaster", team, nickname, socket.id);
+      } else {
+        alert("Нік не може бути пустим");
+      }
+    } else if (playerOrBot === "bot" && team === "blue") {
+      socket.emit("joinMaster", team, "Бот Санчізес", "blueBot");
+    } else socket.emit("joinMaster", team, "Бот Чіназес", "redBot");
+  };
+
+  const joinPlayer = (team) => {
+    const nickname = prompt("Введіть нік:");
+    if (nickname !== null && nickname !== "") {
+      socket.emit("joinPlayer", team, nickname, socket.id);
+    } else {
+      alert("Нік не може бути пустим");
+    }
+  };
+
+  const restartGame = () => {
+    setActiveUsers([]);
+    socket.emit("restartGame");
+  };
+
+  const startGame = () => {
+    setActiveUsers([]);
+    socket.emit("startGame");
+  };
+
+  const isUserMaster = (socketId) => {
+    const user = gameState.users.find((user) => user.id === socketId);
+    return user && user.role === "master";
+  };
+
+  const isUserPlayer = (socketId) => {
+    const user = gameState.users.find((user) => user.id === socketId);
+    return user && user.role === "player";
+  };
+
+  const toggleWordSelection = (index, userId) => {
+    if (!isUserMaster(userId) && isUserPlayer(userId)) {
+      const word = gameState.coloredWords[index];
+
+      if (
+        gameState &&
+        gameState.coloredWords &&
+        gameState.coloredWords[index]
+      ) {
+        if (isNaN(word.selectedCount)) {
+          word.selectedCount = 0;
+        }
+        if (word) {
+          socket.emit(
+            "toggleWordSelection",
+            gameState,
+            index,
+            activeUsers,
+            activeTeam
+          );
+        }
+      }
+    }
+  };
+
+  const chekingWhoClicked = (userId) => {
+    socket.emit("buttonClicked", userId);
+  };
+
+  const showWhoClicked = (userId) => {
+    return activeUsers.includes(userId) ? "active" : "";
+  };
+
+  const submitAssociation = (color) => {
+    socket.emit("submitAssociation", gameState, associationWord, color);
+    setAssociationWord("");
+  };
+
+  useEffect(() => {
+    socket.on("changeMasterControle", (masterControle) => {
+      setShowMasterControls(masterControle);
+    });
+    return () => socket.off("changeMasterControle");
+  }, []);
+
+  const changeActiveTeam = () => {
+    socket.emit("changeActiveTeam", activeTeam, gameState, "add");
+  };
+
+  const firstAssociation = () => {
+    socket.emit("firstAssociation", gameState);
+  };
+
+  const handleButtonClick = () => {
+    socket.emit("changeMasterControle", false);
+  };
+
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={increment}>Увеличить</button>
-      <button onClick={clearCount}>Очистить поле</button>
+    <div className="App">
+      <h1>Codenames Game by Artem Zhowtovatiuk</h1>
+      <div className="role-selection">
+        <button onClick={() => joinMaster("red", "player")}>
+          Join Red Master
+        </button>
+        <button onClick={() => joinMaster("blue", "player")}>
+          Join Blue Master
+        </button>
+        <button onClick={() => joinPlayer("red")}>Join Red Player</button>
+        <button onClick={() => joinPlayer("blue")}>Join Blue Player</button>
+        <button onClick={() => joinMaster("red", "bot")}>
+          Join Red Bot Master
+        </button>
+        <button onClick={() => joinMaster("blue", "bot")}>
+          Join Blue Bot Master
+        </button>
+        <button onClick={changeActiveTeam}>Switch Active Team</button>
+      </div>
+      <div className="restart-game">
+        <button onClick={restartGame}>Restart Game</button>
+      </div>
+      {!isGameStarted && <button onClick={startGame}>Start Game</button>}
+      {gameState && gameState.coloredWords && isGameStarted && (
+        <div className="game-board">
+          <div className="main-row">
+            {/*КОМАНДА ЧЕРВОНИХ*/}
+            <div className={`team red`}>
+              <div className="players">
+                {gameState.users
+                  .filter(
+                    (user) => user.team === "red" && user.role === "master"
+                  )
+                  .map((user) => (
+                    <div key={user.id}>{user.nickname} (Майстер)</div>
+                  ))}
+                {gameState.users
+                  .filter(
+                    (user) => user.team === "red" && user.role === "player"
+                  )
+                  .map((user) => (
+                    <div key={user.id} className={showWhoClicked(user.id)}>
+                      {user.nickname}
+                    </div>
+                  ))}
+              </div>
+              <div className="associations">
+                <div className="associations-words">
+                  <ul>
+                    {gameState.associations &&
+                      gameState.associations["red"].map(
+                        (association, index) => (
+                          <li key={index}>{association}</li>
+                        )
+                      )}
+                  </ul>
+                </div>
+                {gameState.users
+                  .filter(
+                    (user) =>
+                      user.id === socket.id &&
+                      user.role === "master" &&
+                      user.team === "red"
+                  )
+                  .map(
+                    (user) =>
+                      activeTeam === "red" &&
+                      showMasterControls && (
+                        <div className="master-controls" key={user.id}>
+                          <input
+                            type="text"
+                            value={associationWord}
+                            onChange={(e) => setAssociationWord(e.target.value)}
+                            placeholder="Введіть слово"
+                          />
+                          <button
+                            onClick={() => {
+                              submitAssociation("red");
+                              if (!gameState.associations) {
+                                firstAssociation();
+                              }
+                              handleButtonClick();
+                            }}
+                          >
+                            Додати
+                          </button>
+                        </div>
+                      )
+                  )}
+                {activeTeam === "blue" ||
+                  (activeTeam !== "blue" && (
+                    <div className="timer">{timer} сек</div>
+                  ))}
+                <p className="words-left-red">
+                  {
+                    gameState.coloredWords.filter(
+                      (word) => word.color === "red" && word.guessed === false
+                    ).length
+                  }
+                </p>
+              </div>
+            </div>
+            {/*ПОЛЕ ЗІ СЛОВАМИ*/}
+            <div className="words-field">
+              <div className="words">
+                {gameState.coloredWords.map((words, index) => (
+                  <div
+                    key={index}
+                    className={`word ${
+                      isUserMaster(socket.id)
+                        ? `word-${words.color}`
+                        : words.guessed
+                        ? `word-${words.color}`
+                        : ""
+                    }`}
+                  >
+                    <div
+                      onClick={() => {
+                        if (
+                          isUserPlayer(socket.id) &&
+                          !words.guessed &&
+                          gameState.users.find((user) => user.id === socket.id)
+                            .team === activeTeam
+                        ) {
+                          toggleWordSelection(index, socket.id);
+
+                          chekingWhoClicked(socket.id);
+                        }
+                      }}
+                      lang="uk"
+                      className={`word-box`}
+                    >
+                      <p lang="uk">{words.word.word}</p>
+                      {words.selectedCount > 0 && (
+                        <div className="selected-count">
+                          <p>{words.selectedCount}</p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/*КОМАНДА СИНІХ*/}
+            <div className={`team blue`}>
+              <div className="players">
+                {gameState.users
+                  .filter(
+                    (user) => user.team === "blue" && user.role === "master"
+                  )
+                  .map((user) => (
+                    <div key={user.id}>{user.nickname} (Майстер)</div>
+                  ))}
+                {gameState.users
+                  .filter(
+                    (user) => user.team === "blue" && user.role === "player"
+                  )
+                  .map((user) => (
+                    <div key={user.id} className={showWhoClicked(user.id)}>
+                      {user.nickname}
+                    </div>
+                  ))}
+              </div>
+              <div className="associations">
+                <div className="associations-words">
+                  <ul>
+                    {gameState.associations &&
+                      gameState.associations["blue"].map(
+                        (association, index) => (
+                          <li key={index}>{association}</li>
+                        )
+                      )}
+                  </ul>
+                </div>
+                {gameState.users
+                  .filter(
+                    (user) =>
+                      user.id === socket.id &&
+                      user.role === "master" &&
+                      user.team === "blue"
+                  )
+                  .map(
+                    (user) =>
+                      activeTeam === "blue" &&
+                      showMasterControls && (
+                        <div className="master-controls" key={user.id}>
+                          <input
+                            type="text"
+                            value={associationWord}
+                            onChange={(e) => setAssociationWord(e.target.value)}
+                            placeholder="Введіть слово"
+                          />
+                          <button
+                            onClick={() => {
+                              submitAssociation("blue");
+                              if (!gameState.associations) {
+                                firstAssociation();
+                              }
+                              handleButtonClick();
+                            }}
+                          >
+                            Додати
+                          </button>
+                        </div>
+                      )
+                  )}
+                {activeTeam === "red" ||
+                  (activeTeam !== "red" && (
+                    <div className="timer">{timer} сек</div>
+                  ))}
+                <p className="words-left-blue">
+                  {
+                    gameState.coloredWords.filter(
+                      (word) => word.color === "blue" && word.guessed === false
+                    ).length
+                  }
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
